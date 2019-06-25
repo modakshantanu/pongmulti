f490035bc5dd01db4379665fd430d0cf03868859
modakshantanu <modakshantanu@gmail.com>
2019-06-25 12:17:39 +0800

    Ball moves

--- src/gameObjects/Ball.js
+++ src/gameObjects/Ball.js
@@ -23,45 +23,20 @@ class Ball {
         };
     }
 
-//Left or right 
-    ballCollision() { 
-        
-        //Check if the ball will hit the left or right sides of the canvas
-        if (((Ball.y + Ball.speed) <= 0) || ((Ball.y + Ball.speed) >= this.canvas.width)) {
-            Ball.speed = Ball.speed * -1; //If it does, change the speed value to move in the opposite direction
-            Ball.y += Ball.speed; //Move the Ball
-            Ball.x += Ball.gravity;
-        } 
-        else { 
-            //If the ball doesn’t hit the sides, then move the Ball as normal
-            Ball.y += Ball.speed;
-            Ball.x += Ball.gravity;
-        }
-
-        this.draw();// Call the draw function
-    }
-    
-// top and bottom 
-    ballBounce() { 
-        
-        //Check if the ball will hit the top or bottom of the canvas
-        if (((Ball.x + Ball.gravity) <= 0) || ((Ball.x + Ball.gravity + Ball.height) >= this.canvas.height)){
-            Ball.gravity = Ball.gravity * -1; 
-            // If it does, change the gravity value to move in the opposite direction
-            Ball.x += Ball.gravity; // Move the Ball
-            Ball.y += Ball.speed;
-        }
-         else { 
-            //If the ball doesn’t hit the top or bottom, then move the Ball as normal
-            Ball.x += Ball.speed;
-            Ball.y += Ball.gravity
-        }
-
-        this.ballCollision();
-    }
-
     render(state){ 
 
+        
+        if (this.x <= 0 || this.x > this.canvas.width) {
+            this.speed = this.speed * -1;
+        }
+
+        if (this.y <= 0 || this.y > this.canvas.height) {
+            this.gravity = this.gravity * -1;
+        }
+
+        this.x += this.speed;
+        this.y += this.gravity
+        
 		
         var ctx = state.context;
         ctx.beginPath();
